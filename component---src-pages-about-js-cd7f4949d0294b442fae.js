b0fa5540f331d5cf2d9d25dc19701337d63a0fc3
Andrew Farquharson <farquharson.a@gmail.com>
2018-11-17 02:23:43 +0000

    Updates

---
+++ component---src-pages-about-js-cd7f4949d0294b442fae.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{122:function(e,t,n){"use strict";n.r(t);var a=n(0),c=n.n(a);t.default=function(){return c.a.createElement("div",{style:{color:"teal"}},c.a.createElement("h1",null,"About Gatsby"),c.a.createElement("p",null,"Such wow. Very React."))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-cd7f4949d0294b442fae.js.map
